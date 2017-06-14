578f57780445bc5647b4748e2a2498f38bbce2c4
dlinah <eng.lina.a92@gmail.com>
2017-06-14 23:18:47 +0200

    fixing a bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,8 +30,6 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Register', component: RegistrationPage },
-      { title: 'Login', component: LoginPage },
       { title: 'Home', component: HomePage },
       { title: 'Add Product', component: ProductNewPage },
       { title: 'Profile', component: UserPage },
